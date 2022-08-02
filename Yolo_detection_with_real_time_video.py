dc8a3d52a4e3aff99e94406748c882a7e7c331fc
Ozzy Gomes <ozzygomes@outlook.com>
2022-08-02 16:10:54 -0300

    Aumento da velocidade de processamento com tyny, mas perda da acurácia

--- Yolo_detection_with_real_time_video.py
+++ Yolo_detection_with_real_time_video.py
@@ -3,7 +3,7 @@ import numpy as np
 import time
 
 #load Yolo
-net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+net = cv2.dnn.readNet("yolov3-tiny.weights", "yolov3-tiny.cfg")
 classes = []
 with open("coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
@@ -14,7 +14,7 @@ output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(255, 0, size=(len(classes), 3))
 
 #loading image
-cap = cv2.VideoCapture("walking.mp4")
+cap = cv2.VideoCapture(0)
 
 
 starting_time = time.time()
@@ -40,11 +40,13 @@ while True:
     boxes = []
 
     for out in outs:
+        #extract only lines with scores greater than confidence 0.5
+        out = out[np.where(out[:, 5:] > 0.5)[0],:]
         for detection in out:
             scores = detection[5: ] 
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if confidence > 0.5:
+            if confidence > 0.5: #era 0.5
                 #object detected
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1]* height)
@@ -71,16 +73,18 @@ while True:
         if i in indexes:
             x, y, w, h = boxes[i]
             label = str(classes[class_ids[i]])
+            confidence = confidences[i]
             color = colors[i]
             cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-            cv2.putText(frame, label, (x, y + 12), font, 0.8, color, 2)
+            cv2.putText(frame, label + " " + str(round(confidence, 2)), (x, y + 12), font, 0.8, color, 1)
+            
 
     #calculando frame por segundo
     elapsed_time = time.time() - starting_time
     fps = frame_id / elapsed_time
 
     #imprimindo o fps a tela 
-    cv2.putText(frame, "FPS: " + str(fps), (10, 30), font, 2, (0, 0, 0), 1)
+    cv2.putText(frame, "FPS: " + str(round(fps, 2)), (10, 40), font, 3, (0, 0, 0), 2)
 
 
     cv2.imshow("Image", frame)
