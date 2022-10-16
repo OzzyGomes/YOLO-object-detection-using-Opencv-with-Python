918208a4eca1b2a3707a74e90be1c3be9590d888
Ozzy Gomes <ozzygomes@outlook.com>
2022-10-16 15:04:01 -0300

    new

--- Yolo_detection_with_real_time_video.py
+++ Yolo_detection_with_real_time_video.py
@@ -3,7 +3,7 @@ import numpy as np
 import time
 
 #load Yolo
-net = cv2.dnn.readNet("yolov3-tiny.weights", "yolov3-tiny.cfg")
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
 classes = []
 with open("coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
@@ -42,6 +42,7 @@ while True:
     for out in outs:
         #extract only lines with scores greater than confidence 0.5
         out = out[np.where(out[:, 5:] > 0.5)[0],:]
+
         for detection in out:
             scores = detection[5: ] 
             class_id = np.argmax(scores)
